dff22b0d0167facf96b52efad6a74407d4b5e767
Ondrej Sika <ondrej@ondrejsika.com>
2019-12-24 10:50:37 +0100

    Print method params

--- server.py
+++ server.py
@@ -9,11 +9,16 @@ app = Flask(__name__)
 
 @app.route("/v2/domains/<domain>/records", methods=["POST"])
 def domains_create_record(domain):
+    print(
+        'CREATE RECORD "%s" type="%s" name="%s" value="%s"'
+        % (domain, request.json["type"], request.json["name"], request.json["value"])
+    )
     return json.dumps({"uid": "rec_" + hashlib.sha256(request.data).hexdigest()[:16]})
 
 
 @app.route("/v2/domains/<domain>/records/<record_id>", methods=["DELETE"])
 def domains_delete_record(domain, record_id):
+    print('DELETE RECORD "%s" (domain="%s")' % (record_id, domain))
     return json.dumps({})
 
 
