e68dcec284962b7f0e8f6287508b20d183b775c9
Ondrej Sika <ondrej@ondrejsika.com>
2019-12-24 11:42:47 +0100

    Add state & get records (more)
    - Add persistent state using datasets (sqlite3)
    - Add get records mock
    - Run app in single thread
    - Enable debug server

--- server.py
+++ server.py
@@ -1,10 +1,30 @@
 import os
 import hashlib
 import json
+import dataset
 from flask import Flask, request
 
 
 app = Flask(__name__)
+db = dataset.connect("sqlite:///db.sqlite3")
+
+
+@app.route("/v2/domains/<domain>/records", methods=["GET"])
+def domains_get_records(domain):
+    print('GET RECORD "%s"' % (domain))
+    return json.dumps(
+        {
+            "records": [
+                {
+                    "id": rec["uid"],
+                    "name": rec["name"],
+                    "type": rec["type"],
+                    "value": rec["value"],
+                }
+                for rec in db["records"].find()
+            ]
+        }
+    )
 
 
 @app.route("/v2/domains/<domain>/records", methods=["POST"])
@@ -13,14 +33,23 @@ def domains_create_record(domain):
         'CREATE RECORD "%s" type="%s" name="%s" value="%s"'
         % (domain, request.json["type"], request.json["name"], request.json["value"])
     )
-    return json.dumps({"uid": "rec_" + hashlib.sha256(request.data).hexdigest()[:16]})
+    uid = "rec_" + hashlib.sha256(request.data).hexdigest()[:16]
+
+    record = {}
+    record.update({"uid": uid, "domain": domain})
+    record.update(request.json)
+    db["records"].insert(record)
+
+    return json.dumps({"uid": uid})
 
 
 @app.route("/v2/domains/<domain>/records/<record_id>", methods=["DELETE"])
 def domains_delete_record(domain, record_id):
+    db["records"].delete(domain=domain, uid=record_id)
     print('DELETE RECORD "%s" (domain="%s")' % (record_id, domain))
     return json.dumps({})
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port="80")
+    app.debug = True
+    app.run(host="0.0.0.0", port="80", threaded=False, processes=1)
